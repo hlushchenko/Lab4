849ff24c7bfa88619e41761a08a0e227508e634b
Aleksey <alekseyhonchar@gmail.com>
2021-04-15 19:33:40 +0300

    Utilities.cs added

---
+++ Lab4/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Utilities
+    {
+        public static void OutText(string str = "", bool upper = false, bool lower = false)
+        {
+            if (upper) OutEquality();
+            if (str != "") Console.WriteLine(str);
+            if (lower) OutEquality();
+        }
+
+        private static void OutEquality(int count = 30)
+        {
+            for (int i = 0; i < count; i++) Console.Write('=');
+            Console.Write('\n');
+        }
+    }
+}
