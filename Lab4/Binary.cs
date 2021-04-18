c09760971e0622f76e23c0e820277261c42f72e1
Hluushchenko <hluushchenko@gmail.com>
2021-04-18 15:41:14 +0300

    intToBits() added

--- Lab4/Binary.cs
+++ Lab4/Binary.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Lab4
 {
@@ -28,6 +30,30 @@ namespace Lab4
             {
                 bw.Write(data);
             }
+        }
+
+        public void Write(uint[] data)
+        {
+            Write(IntsToBytes(data));
+        }
+
+        private byte[] IntsToBytes(uint[] ints)
+        {
+            List<byte> output = new List<byte>();
+            byte currentSize = 8;
+            
+            return null;
+        }
+
+        public static bool[] IntToBits(uint n)
+        {
+            List<bool> bits = new List<bool>();
+            while (n > 0)
+            {
+                bits.Add(n%2 != 0);
+                n /= 2;
+            }
+            return bits.ToArray();
         }
     }
 }
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -6,9 +6,7 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            var file1 = new Binary("D:/пример.bmp");
-            var file2 = new Binary("D:/пример2.bmp");
-            file2.Write(file1.Read());
+            var a = Binary.IntToBits(8);
         }
     }
 }
