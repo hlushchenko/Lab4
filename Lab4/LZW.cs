ef2d81bfeeef9f3eeb3c7e3471bd852515eb6ad8
Aleksey <alekseyhonchar@gmail.com>
2021-04-18 21:02:45 +0300

    Decoding string

--- Lab4/LZW.cs
+++ Lab4/LZW.cs
@@ -36,11 +36,40 @@ namespace Lab4
             return result;
         }
 
+        public static string Decode(uint[] compressed)
+        {
+            Dictionary<uint, string> dictionary = CreateDictionaryDecode();
+            string decompressed = dictionary[compressed[0]];
+            string w = dictionary[compressed[0]];
+            compressed = Utilities.RemoveByIndex(ref compressed, 0);
+            
+            foreach (int k in compressed)
+            {
+                string entrance = "";
+                if (dictionary.ContainsKey((uint)k))
+                    entrance = dictionary[(uint)k];
+                else if (k == dictionary.Count)
+                    entrance = w + w[0];
+                decompressed += entrance;
+                dictionary.Add((uint)dictionary.Count, w + entrance[0]);
+                w = entrance;
+            }
+            return decompressed;
+        }
+
         public static Dictionary<string, uint> CreateDictionaryEncode()
         {
             Dictionary<string, uint> dictionary = new Dictionary<string, uint>();
             for (int i = 0; i < 256; i++)
                 dictionary.Add(((char)i).ToString(), (uint)i);
+            return dictionary;
+        }
+
+        public static Dictionary<uint, string> CreateDictionaryDecode()
+        {
+            Dictionary<uint, string> dictionary = new Dictionary<uint, string>();
+            for (int i = 0; i < 256; i++)
+                dictionary.Add((uint)i, ((char)i).ToString());
             return dictionary;
         }
     }
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -7,7 +7,8 @@ namespace Lab4
         static void Main(string[] args)
         {
             byte[] arr = { 12, 13, 14, 16, 10, 12, 14};
-            uint[] test = LZW.Encode("ABRACADABRBRABRABRA");
+            uint[] test = LZW.Encode("ABRACADABRABRABRABRA");
+            string res = LZW.Decode(test);
         }
     }
 }
